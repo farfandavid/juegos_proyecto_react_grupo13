3f7da9ef862cdbee8e3bf8770ffacbe6a79b17a3
alv4rokepasowachin <yaskariri2015@gmail.com>
2022-09-24 21:50:32 -0300

    nuevos cambios AlvaroBenencia

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Form } from "react-bootstrap";
+import header from "../pages";
+class Home extends React.Component{
+    render(){
+        return
+      
+    }
+}
+
+export default Home;
+ 
